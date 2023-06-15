16af2ef953ae5002f24d4259a83a1ffb1e0d13e0
Rodrigo Pessanha <rodrigorospe@outlook.com>
2023-06-15 13:51:37 -0300

    Projeto Iniciado

---
+++ Deposito de Produtos/src/main/java/br/edu/iff/bsi/deposito_de_produtos/DepositoDeProdutosApplication.java
@@ -0,0 +1,13 @@
+package br.edu.iff.bsi.deposito_de_produtos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepositoDeProdutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepositoDeProdutosApplication.class, args);
+	}
+
+}
---
+++ Deposito de Produtos/src/test/java/br/edu/iff/bsi/deposito_de_produtos/DepositoDeProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.iff.bsi.deposito_de_produtos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepositoDeProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
