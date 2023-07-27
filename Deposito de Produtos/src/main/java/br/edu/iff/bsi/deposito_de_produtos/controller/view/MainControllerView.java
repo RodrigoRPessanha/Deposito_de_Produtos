ae5875f738ea05c57ff2383cac6c8b2be38a415b
Rodrigo Pessanha <rodrigorospe@outlook.com>
2023-07-27 19:45:41 -0300

    Fragmentos no HTML Setor

--- Deposito de Produtos/src/main/java/br/edu/iff/bsi/deposito_de_produtos/controller/view/MainControllerView.java
+++ Deposito de Produtos/src/main/java/br/edu/iff/bsi/deposito_de_produtos/controller/view/MainControllerView.java
@@ -2,14 +2,16 @@ package br.edu.iff.bsi.deposito_de_produtos.controller.view;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(path = "/home")
 public class MainControllerView {
-    @GetMapping()
+    @GetMapping(path = "/home")
     public String page(){
         return "layoutBase";
     }
 
+    @GetMapping(path = "/Setor")
+    public String Setor(){
+        return "Setor";
+    }
 }
